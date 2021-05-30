cf354002a4a8a5bb19fa7d8e623477fad63eeb74
“polrb5” <polreigbroto@gmail.com>
2021-05-30 09:58:24 +0200

    First Commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const colores = (rojo, verde, azul) => {
+  console.log(
+    `El color tiene ${rojo} rojo, ${verde} de verde y ${azul} de azul, y su regla CSS es: rgb(${rojo}, ${verde}, ${azul})`
+  );
+};
+
+const rojo = 38;
+const verde = 205;
+const azul = 157;
+
+console.log(colores(rojo, verde, azul));
